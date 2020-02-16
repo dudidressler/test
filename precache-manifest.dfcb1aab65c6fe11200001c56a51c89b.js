6ef2c7935c37068b6f93a447f645a1a3a336f9b6
dudidressler <dudi68099@gmail.com>
2020-02-16 20:52:33 +0200

    Updates

---
+++ precache-manifest.dfcb1aab65c6fe11200001c56a51c89b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5eaf5474410a089191ad642d355af3",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "8b7f6188979a746b3b31",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9c118e9da81ccf8556dd",
+    "url": "/test/static/js/2.f0602b86.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/test/static/js/2.f0602b86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b7f6188979a746b3b31",
+    "url": "/test/static/js/main.cceaf5c1.chunk.js"
+  },
+  {
+    "revision": "662944eeb1e9f5751128",
+    "url": "/test/static/js/runtime-main.976bc85b.js"
+  }
+]);
